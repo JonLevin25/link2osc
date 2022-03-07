c9ed6c4ab9ea33af9ee547eac7a5e34d61850dc7
Jon Levin <jon.levin25@gmail.com>
2022-03-07 21:15:45 +0200

    add osc-port/osc-host flags

--- osc/osc_handler.cpp
+++ osc/osc_handler.cpp
@@ -2,18 +2,31 @@
 #include "lo/lo.h"
 #include "lo/lo_cpp.h"
 
+#include <gflags/gflags.h>
+
 // NULL is same as localhost for liblo
 #define BROADCAST_HOST "196.168.1.255"
 // #define DEFAULT_HOST "localhost"
-#define DEFAULT_HOST BROADCAST_HOST
-#define DEFAULT_PORT "7770"
 #define OSC_PATH "/link2osc/update"
 
-lo_address addr = lo_address_new(DEFAULT_HOST, DEFAULT_PORT);
+DEFINE_string(osc_port, "4560", "OSC port");
+DEFINE_string(osc_host, BROADCAST_HOST, "OSC port");
+
+
+lo_address addr;
+
+void init_lo_addr()
+{
+  static bool was_init = false;
+  if (was_init) return;
+
+  addr = lo_address_new(FLAGS_osc_host.c_str(), FLAGS_osc_port.c_str());
+}
 
 // TODO: get host/port from cmdline args
 void osc_send(std::string response)
 {
+  init_lo_addr();
   printf("OSC: (%s:%s/%s):\n", lo_address_get_hostname(addr), lo_address_get_port(addr), OSC_PATH);
   printf("%s\n", response.c_str());
 
